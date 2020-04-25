4fbf27dd1969dc4ca0c1abb3e708d01b63a124c8
Sarang <sarangdp@gmail.com>
2020-04-25 12:10:22 -0400

    Added Data models. Closes #2

---
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,24 @@
+package guru.springframework.msscbeerservice.web.model;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+}
